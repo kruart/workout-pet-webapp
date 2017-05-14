2cdef930cd0c1fff827bb607f8de629a7104fa4a
weoz <weoz@ukr.net>
2017-05-14 20:54:09 +0300

    add Approach entity

---
+++ src/main/java/ua/kruart/workout/model/Approach.java
@@ -0,0 +1,86 @@
+package ua.kruart.workout.model;
+
+import java.time.LocalTime;
+
+/**
+ * Describes one physical approach during the exercise
+ *
+ * @author kruart on 14.05.2017.
+ */
+public class Approach {
+
+    /**
+     * Unique entity identifier
+     */
+    private int id;
+
+    /**
+     * Repeats in one approach
+     */
+    private int repeats;
+
+    /**
+     * Tonnage of the current approach
+     */
+    private float weight;
+
+    /**
+     * Distance of the current approach
+     */
+    private float distance;
+
+    /**
+     * Time of the current approach
+     */
+    private LocalTime time;
+
+    public Approach() {}
+
+    public Approach(int id, int repeats, float weight, float distance, LocalTime time) {
+        this.id = id;
+        this.repeats = repeats;
+        this.weight = weight;
+        this.distance = distance;
+        this.time = time;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getRepeats() {
+        return repeats;
+    }
+
+    public void setRepeats(int repeats) {
+        this.repeats = repeats;
+    }
+
+    public float getWeight() {
+        return weight;
+    }
+
+    public void setWeight(float weight) {
+        this.weight = weight;
+    }
+
+    public float getDistance() {
+        return distance;
+    }
+
+    public void setDistance(float distance) {
+        this.distance = distance;
+    }
+
+    public LocalTime getTime() {
+        return time;
+    }
+
+    public void setTime(LocalTime time) {
+        this.time = time;
+    }
+}
--- src/main/java/ua/kruart/workout/model/Exercise.java
+++ src/main/java/ua/kruart/workout/model/Exercise.java
@@ -10,7 +10,7 @@ import java.util.List;
 public class Exercise {
 
     /**
-     * Unique identifier
+     * Unique entity identifier
      */
     private int id;
 
@@ -30,7 +30,71 @@ public class Exercise {
     private ExerciseDescription description;
 
     /**
+     * Describes each approach of the exercise
+     */
+    private List<Approach> approaches;
+
+    /**
      * Comment of the exercise
      */
     private String comment;
+
+    public Exercise() {}
+
+    public Exercise(int id, List<String> images, ExerciseConfiguration conf, ExerciseDescription description, List<Approach> approaches, String comment) {
+        this.id = id;
+        this.images = images;
+        this.conf = conf;
+        this.description = description;
+        this.approaches = approaches;
+        this.comment = comment;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public List<String> getImages() {
+        return images;
+    }
+
+    public void setImages(List<String> images) {
+        this.images = images;
+    }
+
+    public ExerciseConfiguration getConf() {
+        return conf;
+    }
+
+    public void setConf(ExerciseConfiguration conf) {
+        this.conf = conf;
+    }
+
+    public ExerciseDescription getDescription() {
+        return description;
+    }
+
+    public void setDescription(ExerciseDescription description) {
+        this.description = description;
+    }
+
+    public List<Approach> getApproaches() {
+        return approaches;
+    }
+
+    public void setApproaches(List<Approach> approaches) {
+        this.approaches = approaches;
+    }
+
+    public String getComment() {
+        return comment;
+    }
+
+    public void setComment(String comment) {
+        this.comment = comment;
+    }
 }
