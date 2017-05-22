b93e7af7610ebb1c31b9a6cd7c7a5abcc7020b7c
weoz <weoz@ukr.net>
2017-05-22 18:27:02 +0300

    Adding WorkoutRepository interface

---
+++ src/main/java/ua/kruart/workout/repository/WorkoutRepository.java
@@ -0,0 +1,39 @@
+package ua.kruart.workout.repository;
+
+import ua.kruart.workout.model.Workout;
+
+import java.util.List;
+
+/**
+ *  Defines CRUD methods to access {@link Workout} objects in the persistent storage
+ *
+ * @author kruart on 22.05.2017.
+ */
+public interface WorkoutRepository {
+
+    /**
+     * Saves(creates or modifies) specified workout instance
+     * @param workout object which is necessary to save or update
+     */
+    void save(Workout workout);
+
+    /**
+     * Returns workout with specified identifier. If no workout exists with such identifier
+     * then null is returned
+     * @param workoutId
+     * @return
+     */
+    Workout findById(int workoutId);
+
+    /**
+     * Delete workout with specified identifier
+     * @param workoutId
+     */
+    void delete(int workoutId);
+
+    /**
+     * Returns all the workout objects
+     * @return
+     */
+    List<Workout> findAll();
+}
