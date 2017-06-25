d3316e47910927f439cef66f115db67814a38dd6
weoz <weoz@ukr.net>
2017-06-25 17:29:19 +0300

    creating AbstractControllerTest class

---
+++ src/test/java/ua/kruart/workout/controller/AbstractControllerTest.java
@@ -0,0 +1,37 @@
+package ua.kruart.workout.controller;
+
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+import org.springframework.web.filter.CharacterEncodingFilter;
+
+import javax.annotation.PostConstruct;
+
+/**
+ * Contains a common configuration for all implementations of this class
+ *
+ * Created by kruart on 25.06.2017.
+ */
+@WebAppConfiguration
+@RunWith(SpringJUnit4ClassRunner.class)
+@ActiveProfiles("hsqldb")
+abstract public class AbstractControllerTest {
+
+    protected MockMvc mockMvc;
+
+    @Autowired
+    private WebApplicationContext webApplicationContext;
+
+    @PostConstruct
+    public void postConstruct() {
+        mockMvc = MockMvcBuilders
+                .webAppContextSetup(webApplicationContext)
+                .addFilter(new CharacterEncodingFilter("UTF-8", true))
+                .build();
+    }
+}
