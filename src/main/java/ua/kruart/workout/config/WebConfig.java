b24e3fd8f3a9d12e2d923fbd28031454e8721247
weoz <weoz@ukr.net>
2017-07-25 14:19:20 +0300

    java-config: replace spring-mvc.xml

---
+++ src/main/java/ua/kruart/workout/config/WebConfig.java
@@ -0,0 +1,80 @@
+package ua.kruart.workout.config;
+
+import org.springframework.context.MessageSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ReloadableResourceBundleMessageSource;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.i18n.CookieLocaleResolver;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+import java.util.Collections;
+import java.util.Locale;
+
+/**
+ * Created by kruart on 25.07.2017.
+ */
+@Configuration
+@ComponentScan(basePackages = "ua.kruart.workout.controller")
+@EnableWebMvc
+public class WebConfig extends WebMvcConfigurerAdapter {
+
+    //<mvc:resources mapping="/resources/**" location="/resources/"/>
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/resources/**").addResourceLocations("/resources/");
+    }
+
+    @Bean
+    public MappingJackson2HttpMessageConverter messageConverter() {
+        return new MappingJackson2HttpMessageConverter();
+    }
+
+    @Bean
+    public RestTemplate restTemplate(MappingJackson2HttpMessageConverter messageConverter) {
+        return new RestTemplate(Collections.singletonList(messageConverter));
+    }
+
+    @Bean
+    public ViewResolver viewResolver() {
+        InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
+//        viewResolver.setViewClass(JstlView.class);
+        viewResolver.setPrefix("/WEB-INF/jsp/");
+        viewResolver.setSuffix(".jsp");
+        return viewResolver;
+    }
+
+    @Bean
+    public MessageSource messageSource() {
+        ReloadableResourceBundleMessageSource resource = new ReloadableResourceBundleMessageSource();
+        resource.setDefaultEncoding("UTF-8");
+        resource.setBasename("classpath:locales/messages");
+        //change locale on default locale. Spring will search file messages.properties - is default locale
+        resource.setFallbackToSystemLocale(false);
+        return resource;
+    }
+
+    @Bean
+    public LocaleResolver localeResolver() {
+        CookieLocaleResolver resolver = new CookieLocaleResolver();
+        resolver.setDefaultLocale(new Locale("uk"));
+        return resolver;
+    }
+
+    //mvc:interceptors
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        LocaleChangeInterceptor interceptor = new LocaleChangeInterceptor();
+        interceptor.setParamName("lang");
+        registry.addInterceptor(interceptor);
+    }
+}
